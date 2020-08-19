3ecde7c6790ec2070db4a65c3dcc3f9d292ae705
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-08-19 15:00:24 +0530

    setup express and created one controller with router

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
+app.get('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`error in setting up the server ${err}`);
