f4975c25e04dc9eab8edb4e6ccfb1b4a037a2662
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-08-22 21:52:25 +0530

    Set up view engine EJS

---
+++ controllers/user_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile = function(req, res){
+    console.log('hey');
+    return res.end('<h1>User profile</h1>')
+}
--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 8000;
 
 // use express router
-app.get('/', require('./routes'));
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 
 app.listen(port, function(err){
     if(err){
