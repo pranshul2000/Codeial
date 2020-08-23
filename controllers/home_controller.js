b48ed2c84502caedf4ab7095f819295d7ed35f6f
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-08-23 16:37:34 +0530

    creaet post and display on home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,21 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "codeial | Home",
+            posts: posts
+        });
+    })
+
+    
 }
