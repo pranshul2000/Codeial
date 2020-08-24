c56dcb31e1ffd55b9d44756249b80acc0b7be8d2
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-08-24 22:46:02 +0530

    create and delete post dynamically

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,17 +4,26 @@ const Comment = require('../models/comment');
 module.exports.create = async function (req, res) {
 
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
 
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            })
+        }
+
+        req.flash('success', 'Post created!');
         return res.redirect('/');
     } catch (err) {
-        console.log('error', err);
+        req.flash('error', err);
         return;
     }
-
 
 }
 
@@ -26,16 +35,25 @@ module.exports.destroy = async function (req, res) {
         if (post.user == req.user.id) {
             post.remove();
             await Comment.deleteMany({ post: req.params.id });
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted!"
+                })
+            }
+
+            req.flash('success', 'Post deleted!');
             return res.redirect('back');
         } else {
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
 
     } catch (err) {
-        console.log('error',err);
+        req.flash('error', err);
         return;
-
     }
-
-
 }
