e1f87a96a9d84d1dc67c6459605c32520db07cd3
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-08-26 13:49:36 +0530

    set up nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+
+let transpoter = nodemailer.createTransport({
+    service: 'gamil',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'testmailer.pg',
+        pass: 'pranshul.mailer'
+    }
+
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath ),
+        data,
+        function(err, template){
+            if(err){console.log('error in rendering templete', err);return;}
+
+            mailHTML = template;
+
+        }
+    )
+
+    return mailHTML;
+}
+
+module.exports = {
+    transpoter: transpoter,
+    renderTemplate: renderTemplate
+}
