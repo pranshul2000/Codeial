dfe99ad3cc64af913707bf9442dc71e998514539
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-09-02 15:13:48 +0530

    beginning the initial connection of socket.io

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,11 @@
 module.exports.chatScokets = function(socketServer){
-    
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection recived', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('Scoket Disconnected!!!!!!');
+        })
+    })
 }
