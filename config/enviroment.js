787f40d04456fbfe5453c48ac1aa23a70db87591
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-09-03 16:16:16 +0530

    set up for production

--- config/enviroment.js
+++ config/enviroment.js
@@ -11,20 +11,43 @@ const develpoment = {
         secure: false,
         auth: {
             user: 'testmailer.pg',
-            pass: 'pranshul.mailer'
+            pass: 'pranshul'
         }
     },
     google_client_id: "960338923443-n71fpl294f9c6lp0j0kashh4jb39mr0r.apps.googleusercontent.com",
-    google_client_secret: "rDCEYEAKkSEAMvy_XhjUy6kQ",
+    google_client_secret: "",
     google_callback_url: "http://localhost:8000/users/auth/google/callback",
     facebook_clint_id: '332539968117863',
-    facebook_clint_secret: '614de681bbeeb681f30c8d8591cb069d',
+    facebook_clint_secret: '',
     facebook_callback_url: 'http://localhost:8000/users/auth/facebook/callback',
     jwt_secret: 'codeial'
 }
 
 const production = {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.codeial_asset_path,
+    session_cookie_key: process.env.codeial_session_cookie_key,
+    db: 'codeial_production',
+    smtp: {
+        service: 'gamil',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user:process.env.codeial_smtp_auth_user,
+            pass: process.env.codeial_smtp_auth_pass
+        }
+    },
+    google_client_id: process.env.codeial_google_client_id,
+    google_client_secret: process.env.codeial_google_client_secret,
+    google_callback_url: process.env.codeial_google_callback_url,
+    facebook_clint_id: process.env.codeial_facebook_clint_id,
+    facebook_clint_secret: process.env.codeial_facebook_clint_secret,
+    facebook_callback_url: process.env.codeial_facebook_callback_url,
+    jwt_secret: process.env.codeial_jwt_secret
 }
 
-module.exports = develpoment;
+// module.exports = eval(process.env.NODE_ENV) == undefined ? develpoment: eval(process.env.NODE_ENV);
+
+module.exports = eval(process.env.NODE_ENV)==undefined?development:eval(process.env.NODE_ENV);
+// module.exports = development;
