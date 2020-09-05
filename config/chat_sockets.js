612fd55bd50684855484f1c9df99085e77efff70
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-09-05 17:34:23 +0530

    changes... temp

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,27 +1,28 @@
-module.exports.chatSockets=function(chatServer)
-{
-    let io=require('socket.io')(chatServer);
-    io.sockets.on('connection', function(socket)
-    {
-        console.log('New Connection Recieved!', socket.id);
 
+module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer);
 
-        socket.on('disconnect', function()
-        {
-            console.log('Socket Disconnected!');
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected!');
         });
 
+        
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
 
-        socket.on('join_room', function(data)
-        {
-            console.log('joining request recieved!', data);
             socket.join(data.chatroom);
+
             io.in(data.chatroom).emit('user_joined', data);
         });
 
-        socket.on('send_message', function(data)
-        {
-            io.in(data.chatroom).emit('recieve_message', data);
-        })
-    })
+        // CHANGE :: detect send_message and broadcast to everyone in the room
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+
+    });
+
 }
