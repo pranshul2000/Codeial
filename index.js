a260cff28893e6eee1fd3a1e799ab9f1b55aef87
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-09-05 17:36:21 +0530

    few changes required

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 // set up the chat server to be used with socket.io
 const chatServer = require('http').Server(app);
-const chatScokets = require('./config/chat_sockets').chatScokets(chatServer);
+const chatScokets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port: 5000');
 const path = require('path');
